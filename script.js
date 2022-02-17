2382f47d325860171801f6d14fa3fc0fd5c256aa
mathos <bretmathilde@orange.fr>
2022-02-17 18:52:29 +0100

    commit

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function addItemToCart(cart1, cart2, cart3) {
 
 
 
-/* CHOIX 1 : LA PATE */
+// CHOIX 1 : LA PATE
 
 var un = document.getElementById("un");
 var li1 = un.querySelectorAll("ul > li");
@@ -62,7 +62,7 @@ function ajoutCart(title) {
     var cart1 = document.getElementById('cart1')
     var cartRowContents = 
     `
-      <span class="cart-item-title">${title}</span>    
+      <span>${title}</span>    
     `
     cartRow.innerHTML = cartRowContents
     cart1.textContent = cartRow.innerText
@@ -76,28 +76,50 @@ function ajoutCart(title) {
 
 // CHOIX 2 : LA GOURMANDISE 
 
-let deux = document.getElementById("deux");
-let li2 = deux.querySelectorAll("ul > li");
-let cart2 = document.getElementById("cart2");
-
-
+var deux = document.getElementById("deux");
+var li2 = deux.querySelectorAll("ul > li");
 
 // On fait une boucle pour attribuer la classe active sur le bouton sélectionné
 for (var i = 0; i < li2.length; i++) {
-  li2[i].addEventListener("click", function() {
-    var active = document.getElementsByClassName("active2");
-
-    // Si la classe active n'est pas déjà associée à une balise :
+  
+  li2[i].addEventListener('click', function() {
+    var active = document.getElementsByClassName("active");
+    // Si la classe active n'est pas déjà associée à une balise
     if (active.length > 2) {
-      this.classList = this.classList.remove("active2");  
+      this.classList = this.classList.remove("active");  
     return;  
     }
     // On ajoute la class active au bouton sur lequel on clique
-    this.classList.toggle("active2");
-    cart2.textContent = this.innerText;
-
-  });
+    this.classList.toggle("active");
+    ajout()
+  }) 
 }
+
+function ajout() {
+  var button = li2[i]
+    var button = event.target
+    var shopItem = button.parentElement.parentElement
+    console.log(shopItem)
+    var title = shopItem.getElementsByClassName("prout")[0].innerText
+    console.log(title)
+    ajoutCart(title)
+}
+
+function ajoutCart(title) {
+    var cartRow = document.createElement('div')
+    var cart2 = document.getElementById('cart2')
+    var cartRowContents = 
+    `
+      <span>${title}</span>    
+    `
+    cartRow.innerHTML = cartRowContents
+    cart2.textContent = cartRow.innerText
+    /*cart2.append(cartRow) */
+    /*Utiliser la méthode append pour le bloc deux pour autoriser
+    sélectionner plusieurs div */
+
+}
+
 
 
 // CHOIX 3 : LE CHOCOLAT
